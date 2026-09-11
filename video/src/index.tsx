9c6708d9aaff9564e370d818ed651b445c786ce1
drained <christopherakwuba4@gmail.com>
2026-09-11 05:03:31 +0100

    cleared repo

--- video/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { registerRoot } from "remotion";
-import { Composition } from "remotion";
-import { PraetorDemo } from "./PraetorDemo";
-
-registerRoot(() => (
-  <Composition id="PraetorDemo" component={PraetorDemo} durationInFrames={2220} fps={30} width={1920} height={1080} />
-));
